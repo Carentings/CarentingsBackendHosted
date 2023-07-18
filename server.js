ae19ff02230209d8ff1c8a9c3372b16ef21de35b
Carentings <69449720+Carentings@users.noreply.github.com>
2023-07-18 12:52:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,16 @@ const swaggerUi = require("swagger-ui-express");
 const app = express();
 
 app.use(express.json());
+const cors = require("cors");
+const app = express();
 
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
+  })
+);
 
 const swaggerDefinition = {
   openapi: "3.0.0",
